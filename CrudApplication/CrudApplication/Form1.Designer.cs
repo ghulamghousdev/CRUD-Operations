f0e6d7892401049459df57bf7a7cd2830f504313
ghulamghousdev <itsghulamghous@gmail.com>
2020-08-24 01:43:02 +0500

    final commit

---
+++ CrudApplication/CrudApplication/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace CrudApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cnicLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cnicColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emailColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contactColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contactLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.contactBox = new System.Windows.Forms.TextBox();
+            this.emailBox = new System.Windows.Forms.TextBox();
+            this.cnicBox = new System.Windows.Forms.TextBox();
+            this.update_button = new System.Windows.Forms.Button();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cnicLabel
+            // 
+            this.cnicLabel.AutoSize = true;
+            this.cnicLabel.Location = new System.Drawing.Point(123, 210);
+            this.cnicLabel.Name = "cnicLabel";
+            this.cnicLabel.Size = new System.Drawing.Size(47, 20);
+            this.cnicLabel.TabIndex = 0;
+            this.cnicLabel.Text = "CNIC";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(123, 167);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(51, 20);
+            this.nameLabel.TabIndex = 3;
+            this.nameLabel.Text = "Name";
+            // 
+            // nameBox
+            // 
+            this.nameBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.nameBox.Location = new System.Drawing.Point(211, 164);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(181, 26);
+            this.nameBox.TabIndex = 7;
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.saveButton.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.Location = new System.Drawing.Point(120, 389);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(82, 44);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = false;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameColumn,
+            this.cnicColumn,
+            this.emailColumn,
+            this.contactColumn});
+            this.dataGridView1.Location = new System.Drawing.Point(567, 164);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(658, 269);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            // 
+            // nameColumn
+            // 
+            this.nameColumn.HeaderText = "Name";
+            this.nameColumn.MinimumWidth = 8;
+            this.nameColumn.Name = "nameColumn";
+            this.nameColumn.Width = 150;
+            // 
+            // cnicColumn
+            // 
+            this.cnicColumn.HeaderText = "CNIC";
+            this.cnicColumn.MinimumWidth = 8;
+            this.cnicColumn.Name = "cnicColumn";
+            this.cnicColumn.Width = 150;
+            // 
+            // emailColumn
+            // 
+            this.emailColumn.HeaderText = "Email";
+            this.emailColumn.MinimumWidth = 8;
+            this.emailColumn.Name = "emailColumn";
+            this.emailColumn.Width = 150;
+            // 
+            // contactColumn
+            // 
+            this.contactColumn.HeaderText = "Contact";
+            this.contactColumn.MinimumWidth = 8;
+            this.contactColumn.Name = "contactColumn";
+            this.contactColumn.Width = 150;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.BackColor = System.Drawing.Color.Red;
+            this.deleteButton.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteButton.Location = new System.Drawing.Point(301, 389);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(91, 44);
+            this.deleteButton.TabIndex = 12;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = false;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.updateButton.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateButton.Location = new System.Drawing.Point(211, 389);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(84, 44);
+            this.updateButton.TabIndex = 13;
+            this.updateButton.Text = "View";
+            this.updateButton.UseVisualStyleBackColor = false;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(294, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 14;
+            // 
+            // contactLabel
+            // 
+            this.contactLabel.AutoSize = true;
+            this.contactLabel.Location = new System.Drawing.Point(123, 291);
+            this.contactLabel.Name = "contactLabel";
+            this.contactLabel.Size = new System.Drawing.Size(65, 20);
+            this.contactLabel.TabIndex = 15;
+            this.contactLabel.Text = "Contact";
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Location = new System.Drawing.Point(126, 253);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(48, 20);
+            this.emailLabel.TabIndex = 16;
+            this.emailLabel.Text = "Email";
+            // 
+            // contactBox
+            // 
+            this.contactBox.Location = new System.Drawing.Point(211, 291);
+            this.contactBox.Name = "contactBox";
+            this.contactBox.Size = new System.Drawing.Size(181, 26);
+            this.contactBox.TabIndex = 17;
+            // 
+            // emailBox
+            // 
+            this.emailBox.Location = new System.Drawing.Point(211, 253);
+            this.emailBox.Name = "emailBox";
+            this.emailBox.Size = new System.Drawing.Size(181, 26);
+            this.emailBox.TabIndex = 18;
+            // 
+            // cnicBox
+            // 
+            this.cnicBox.Location = new System.Drawing.Point(211, 204);
+            this.cnicBox.Name = "cnicBox";
+            this.cnicBox.Size = new System.Drawing.Size(181, 26);
+            this.cnicBox.TabIndex = 19;
+            // 
+            // update_button
+            // 
+            this.update_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.update_button.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.update_button.Location = new System.Drawing.Point(394, 389);
+            this.update_button.Name = "update_button";
+            this.update_button.Size = new System.Drawing.Size(101, 44);
+            this.update_button.TabIndex = 20;
+            this.update_button.Text = "Update";
+            this.update_button.UseVisualStyleBackColor = false;
+            this.update_button.Click += new System.EventHandler(this.update_button_Click);
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(878, 43);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(188, 26);
+            this.searchBox.TabIndex = 21;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(984, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 33);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1338, 679);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.update_button);
+            this.Controls.Add(this.cnicBox);
+            this.Controls.Add(this.emailBox);
+            this.Controls.Add(this.contactBox);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.contactLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.cnicLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label cnicLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label contactLabel;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.TextBox contactBox;
+        private System.Windows.Forms.TextBox emailBox;
+        private System.Windows.Forms.TextBox cnicBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cnicColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emailColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn contactColumn;
+        private System.Windows.Forms.Button update_button;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CrudApplication/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrudApplication
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-UUG2AVV;Initial Catalog=crudApplication;Integrated Security=True");
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataRetrivel();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clearBoxes()
+        {
+
+            nameBox.Text = "";
+            cnicBox.Text = "";
+            emailBox.Text = "";
+            contactBox.Text = "";
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            string name = nameBox.Text;
+            string cnic = cnicBox.Text;
+            string email = emailBox.Text;
+            string contact = contactBox.Text;
+
+             con.Open();
+
+            string firstQuery = "insert into userRecords(Name,CNIC,Email,Contact) values(@fname, @cnic, @email,@contact)";
+            SqlCommand cmd = new SqlCommand(firstQuery, con);
+
+            cmd.Parameters.AddWithValue("@fname", name);
+            cmd.Parameters.AddWithValue("@cnic", cnic);
+            cmd.Parameters.AddWithValue("@email", email);
+            cmd.Parameters.AddWithValue("@contact", contact);
+
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Your Data has been saved😊");
+            clearBoxes();
+            con.Close();
+        }
+
+        private void dataRetrivel() 
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from userRecords", con);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+
+            dataGridView1.Rows.Clear();
+            for(int i=0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                string name = ds.Tables[0].Rows[i].ItemArray[0].ToString();
+                string cnic = ds.Tables[0].Rows[i].ItemArray[1].ToString();
+                string email = ds.Tables[0].Rows[i].ItemArray[2].ToString();
+                string contact = ds.Tables[0].Rows[i].ItemArray[3].ToString();
+
+                DataGridViewRow dataPush = new DataGridViewRow();
+                dataPush.CreateCells(dataGridView1);
+                dataPush.Cells[0].Value = name;
+                dataPush.Cells[1].Value = cnic;
+                dataPush.Cells[2].Value = email;
+                dataPush.Cells[3].Value = contact;
+                dataGridView1.Rows.Add(dataPush);
+            }
+            con.Close();
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            dataRetrivel();
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            int rowIndex = e.RowIndex;
+            DataGridViewRow row = dataGridView1.Rows[rowIndex];
+
+            nameBox.Text = row.Cells[0].Value.ToString();
+            cnicBox.Text = row.Cells[1].Value.ToString();
+            emailBox.Text = row.Cells[2].Value.ToString();
+            contactBox.Text = row.Cells[3].Value.ToString();
+        }
+
+        private void deleteRecords()
+        {
+            con.Open();
+            string id = nameBox.Text;
+
+            SqlCommand cmd = new SqlCommand("delete from userRecords where Name=@a", con);
+            cmd.Parameters.AddWithValue("@a", id);
+            cmd.ExecuteNonQuery();
+
+            MessageBox.Show("GONE");
+            con.Close();
+            dataRetrivel();
+            clearBoxes();
+        }
+
+
+        private void UpdateRecords()
+        {
+            con.Open();
+            string name = nameBox.Text;
+            string cnic = cnicBox.Text;
+            string email = emailBox.Text;
+            string contact = contactBox.Text;
+            
+            SqlCommand cmd = new SqlCommand("update userRecords set CNIC=@newCNIC, Email=@newEmail, Contact=@newContact where Name=@newName", con);
+            cmd.Parameters.AddWithValue("newName", name);
+            cmd.Parameters.AddWithValue("newContact", contact);
+            cmd.Parameters.AddWithValue("newEmail", email);
+            cmd.Parameters.AddWithValue("newCNIC", cnic);
+
+            cmd.ExecuteNonQuery();
+
+            MessageBox.Show("The Record has been updated!");
+            con.Close();
+            dataRetrivel();
+            clearBoxes();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            deleteRecords();
+        }
+
+
+        private void update_button_Click(object sender, EventArgs e)
+        {
+            UpdateRecords();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            con.Open();
+
+            string Query = "SELECT * FROM userRecords WHERE CONCAT (Name, CNIC, Email, Contact) LIKE '%" + searchBox.Text + "%'";
+            SqlCommand CMD = new SqlCommand(Query, con);
+            SqlDataAdapter DiscordAdpater = new SqlDataAdapter(CMD);
+            DataSet DiscordSet = new DataSet();
+
+            DiscordAdpater.Fill(DiscordSet);
+            dataGridView1.Rows.Clear();
+
+            for (int i = 0; i < DiscordSet.Tables[0].Rows.Count; ++i)
+            {
+                DataGridViewRow Rows = new DataGridViewRow();
+
+                string User_Name = DiscordSet.Tables[0].Rows[i].ItemArray[0].ToString();
+                string User_CNIC = DiscordSet.Tables[0].Rows[i].ItemArray[1].ToString();
+                string User_Contact = DiscordSet.Tables[0].Rows[i].ItemArray[2].ToString();
+                string User_Email = DiscordSet.Tables[0].Rows[i].ItemArray[3].ToString();
+
+                Rows.CreateCells(dataGridView1);
+                Rows.Cells[0].Value = User_CNIC;
+                Rows.Cells[1].Value = User_Name;
+                Rows.Cells[2].Value = User_Contact;
+                Rows.Cells[3].Value = User_Email;
+
+                dataGridView1.Rows.Add(Rows);
+            }
+            con.Close();
+
+
+        }
+    }
+}
